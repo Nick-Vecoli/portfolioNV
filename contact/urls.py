68d8e4568978a9b07d1f3dab12ca982ded819bc7
Nick Vecoli <n.vecoli@gmail.com>
2023-04-10 17:21:07 +1000

    added requirements.txt

--- contact/urls.py
+++ contact/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 
-print("fart")
+
 urlpatterns = [
     path('contact', views.contact, name='contact')
 ]
--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from . import views
-print("fart2")
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('about', views.about, name='about'),
